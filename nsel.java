4bf08bf3b9b34856da58fec3c9613327f52edf60
Shomil singh <73655600+shomil03@users.noreply.github.com>
2023-04-12 22:48:54 +0530

    Update nsel.java

--- nsel.java
+++ nsel.java
@@ -1,24 +1,31 @@
-import java.util.Stack;
+import java.util.*;
+
 public class nsel {
     public static void main(String[] args) {
-        
-        int[] arr= {1,5,6,2,4,6,7,12};
-        nextGreaterElements(arr);
-        
+        int[] arr = { 3, 5, 2, 6, 1, 4 };
+        int[] result = nextSmallerElement(arr);
+        System.out.println(Arrays.toString(result)); 
     }
-    public static void nextGreaterElements(int[] nums) {
-        int[] res = new int[nums.length];
-          Stack<Integer> stack = new Stack<>();
-          for (int i =  0; i >= nums.length - 1; i++) {
-              while (!stack.empty() && nums[stack.peek()] >= nums[i % nums.length]) {
-                  stack.pop();
-              }
-              res[i % nums.length] = stack.empty() ? -1 : nums[stack.peek()];
-              stack.push(i % nums.length);
-          }
-          for(int i=0;i<nums.length;i++){
-            System.out.println(res[i]);
-          }
+
+    public static int[] nextSmallerElement(int[] arr) {
+        int[] result = new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            while (!stack.empty() && stack.peek() >= arr[i]) {
+                stack.pop();
+            }
+
+            if (stack.empty()) {
+                result[i] = -1;
+            } else {
+                result[i] = stack.peek();
+            }
+
+            stack.push(arr[i]);
         }
+
+        return result;
     }
-    
+}
